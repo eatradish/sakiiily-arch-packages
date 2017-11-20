2c20d4a47be59c4cdb5789c2f9dbe2cfd3adab7f
sakiiily <qingbaodou@gmail.com>
2017-11-20 12:42:22 +0800

    del rubblish text

--- config.sh
+++ config.sh
@@ -1,10 +1,8 @@
 /usr/bin/$*
-echo ${0%/*}
 name=package
 HOST=`cat /etc/hostname`
 LOCAL="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "$2" =~ "-S" ]] || [[ "$2" =~ "-R" ]] && [ "$1"="pacaur" ]; then
-  echo "ha"
   pacman -Qnq > "$LOCAL"/$name"-"$HOST".txt"
   pacman -Qmq >> "$LOCAL"/$name"-"$HOST".txt"
   cd $LOCAL
