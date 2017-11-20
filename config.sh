d1d9eb58d25f3506cb49caeb2fb96ffb3369389b
eatradish <qingbaodou@gmail.com>
2017-11-20 19:54:06 +0800

    package list update

--- config.sh
+++ config.sh
@@ -3,10 +3,14 @@ name=package
 HOST=`cat /etc/hostname`
 LOCAL="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [[ "$2" =~ "-S" ]] || [[ "$2" =~ "-R" ]] && [ "$1"="pacaur" ]; then
-  pacman -Qnq > "$LOCAL"/$name"-"$HOST".txt"
-  pacman -Qmq >> "$LOCAL"/$name"-"$HOST".txt"
-  cd $LOCAL
-  git add .
-  git commit -m "package list update"
-  git push
+  if [ "$2"= "-Ss" ]; then
+    return 0
+  else
+    pacman -Qnq > "$LOCAL"/$name"-"$HOST".txt"
+    pacman -Qmq >> "$LOCAL"/$name"-"$HOST".txt"
+    cd $LOCAL
+    git add .
+    git commit -m "package list update"
+    git push
+  fi
 fi
